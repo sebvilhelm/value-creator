07a5ab4e9adef9bfc364db144213e2e5f983903b
Sebastian Vilhelm Nielsen <seb.vilhelm@gmail.com>
2022-08-12 14:49:10 +0200

    feat: add e2e testing

---
+++ tests/value-creator.spec.ts
@@ -0,0 +1,23 @@
+import { test } from "@playwright/test";
+
+test("can create value", async ({ page }) => {
+  // Go to page
+  await page.goto("http://localhost:8787/");
+
+  // Click Create value button
+  await page.locator("text=Create value").click();
+  //
+  // Click text=Enter your name
+  await page.locator("text=Enter your name").click();
+
+  // Fill [placeholder="Enter your name"]
+  await page.locator('[placeholder="Enter your name"]').fill("Sebastian");
+
+  // Press Enter
+  await page.locator('[placeholder="Enter your name"]').press("Enter");
+
+  // Click text=Create value
+  await page.locator("text=Create value").click();
+
+  // TODO: add assertions
+});
